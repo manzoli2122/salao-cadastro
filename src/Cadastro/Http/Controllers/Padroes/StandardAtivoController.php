210236572f9a743fa70800d44a5696e6a08e8c78
Bruno Manzoli do Nascimento <manzoli2122@gmail.com>
2018-02-18 10:11:08 -0300

    30-01-2018

--- src/Cadastro/Http/Controllers/Padroes/StandardAtivoController.php
+++ src/Cadastro/Http/Controllers/Padroes/StandardAtivoController.php
@@ -145,7 +145,7 @@ class StandardAtivoController extends Controller
     * Mais informações em: http://datatables.yajrabox.com
     *
     * @return \Illuminate\Http\JsonResponse
-    */
+    
     public function getDatatable()
     {
         $models = $this->model->getDatatable();
@@ -156,7 +156,24 @@ class StandardAtivoController extends Controller
                     . '<a href="'.route("{$this->route}.show", $linha->id).'" class="btn btn-primary btn-xs" title="Visualizar"> <i class="fa fa-search"></i> </a>';
             })->make(true);
     }
+    */
 
+     /**
+    * Processa a requisição AJAX do DataTable na página de listagem.
+    * Mais informações em: http://datatables.yajrabox.com
+    *
+    * @return \Illuminate\Http\JsonResponse
+    */
+    public function getDatatable()
+    {
+        $models = $this->model->getDatatable();
+        return Datatables::of($models)
+            ->addColumn('action', function($linha) {
+                return '<button data-id="'.$linha->id.'" btn-excluir type="button" class="btn btn-danger btn-xs" title="Excluir"> <i class="fa fa-times"></i> </button> '
+                    . '<a href="'.route("{$this->route}.edit", $linha->id).'" class="btn btn-warning btn-xs" style="margin-left: 10px;" title="Editar"> <i class="fa fa-pencil"></i> </a> '
+                    . '<a href="'.route("{$this->route}.show", $linha->id).'" class="btn btn-primary btn-xs" style="margin-left: 10px;" title="Visualizar"> <i class="fa fa-search"></i> </a>';
+            })->make(true);
+    }
 
 
     
