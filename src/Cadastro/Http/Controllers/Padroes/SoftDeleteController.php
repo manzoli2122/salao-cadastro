dd0c61007c30a83fecfd2f10e90abdbf5735a7e0
Bruno Manzoli do Nascimento <manzoli2122@gmail.com>
2018-02-18 10:47:49 -0300

    30-01-2018

--- src/Cadastro/Http/Controllers/Padroes/SoftDeleteController.php
+++ src/Cadastro/Http/Controllers/Padroes/SoftDeleteController.php
@@ -140,7 +140,7 @@ class SoftDeleteController extends Controller
         return Datatables::of($models)
             ->addColumn('action', function($linha) {
                 return '<button data-id="'.$linha->id.'" btn-excluir type="button" class="btn btn-danger btn-xs" title="Excluir"> <i class="fa fa-times"></i> </button> '
-                    . '<a href="'.route("{$this->route}.edit", $linha->id).'" class="btn btn-primary btn-xs" style="margin-left: 10px;" title="Editar"> <i class="fa fa-pencil"></i> </a> '
+                    . '<a href="'.route("{$this->route}.edit", $linha->id).'" class="btn btn-warning btn-xs" style="margin-left: 10px;" title="Editar"> <i class="fa fa-pencil"></i> </a> '
                     . '<a href="'.route("{$this->route}.show", $linha->id).'" class="btn btn-primary btn-xs" style="margin-left: 10px;" title="Visualizar"> <i class="fa fa-search"></i> </a>';
             })->make(true);
     }
